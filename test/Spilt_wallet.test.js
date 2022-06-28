724b6808cf80229ca87addeb65be80cc55cc0b7e
DkDeepak001 <deepakeswar001@gmail.com>
2022-06-28 07:21:55 +0530

    tested

--- test/Spilt_wallet.test.js
+++ test/Spilt_wallet.test.js
@@ -29,4 +29,35 @@ contract('split_wallet',async (accounts) => {
 		});
 	  });
 
+ it('Should NOT split payment if array length mismatch', async () => {
+    const recipients = [accounts[1], accounts[2], accounts[3]];
+    const amounts = [40, 20];
+    try {
+      await split_wallet.send(
+        recipients,
+        amounts,
+        {from: accounts[0], value: 90} 
+      );
+    } catch (e) {
+      assert(e.message.includes('to must be same length as amount'));
+      return;
+    }
+    assert(false);
+  });
+  it('Should NOT split payment if caller is not owner', async () => {
+    const recipients = [accounts[1], accounts[2], accounts[3]];
+    const amounts = [40, 20, 30];
+    try {
+      await split_wallet.send(
+        recipients,
+        amounts,
+        {from: accounts[5], value: 90} 
+      );
+    } catch (e) {
+      assert(e.message.includes('VM Exception while processing transaction: revert'));
+      return;
+    }
+    assert(false);
+  });
+
 });
